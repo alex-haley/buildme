4fcc51a7798f5879218831f6c1d9f7373410c5f2
alex-haley <mloyygraduate7@gmail.com>
2025-03-24 22:00:46 +0800

    updated test

--- test.c
+++ test.c
@@ -1,13 +1,45 @@
 #include "buildme.c"
 
-int main()
+void debug_build()
 {
-    first.input_path = "main.c";
-    first.output_path = "main.exe";
-    first.compiler_flags = "";
+    first.compiler_flags = "-Od -Oi -Z7 -Wno-writable-strings";
+    first.linker_flags = "";
+
+    BuildProgram();
+}
+
+void release_build()
+{
+    first.compiler_flags = "-O2 -Wno-writable-strings";
     first.linker_flags = "";
     
     BuildProgram();
+}
+
+int scmp(char *a, char *b)
+{
+    for (int x = 0; a[x] == b[x]; ++x)
+	if (!a[x] || ++x >= slen(a)) return 1;
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2) {
+	system("echo \"usage: test [-debug|-release]\"");
+    }
+
+    first.input_path = "hello.c";
+    first.output_path = "hello.exe";
+
+    if (scmp(argv[1], "-release")) {
+	system("echo release");
+	release_build();
+    }
+    if (scmp(argv[1], "-debug")) {
+	system("echo debug");
+	debug_build();
+    }
 
     return 0;
 }
